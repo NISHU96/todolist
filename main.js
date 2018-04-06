291c4e5b7a0217a2f6b0b0d84c035dd4cb2b2402
KUMAR Naresh <naresh.kumar2@ext.soprasteria.com>
2018-04-06 15:09:29 +0530

    minor updates

--- main.js
+++ main.js
@@ -2,18 +2,25 @@ $(document).ready(function(){
     var input = document.getElementById("inputID");
     var button = document.getElementById("buttonID");
     
-
-    function createListItem() {
-        var li = document.createElement("li");
-        li.setAttribute("class","liclass");
+    function createCloseButton() {
         var span = document.createElement("span");
         span.setAttribute("class", "timesclass");
         span.onclick = function() {
             hideListitem(this);
         };
         span.innerHTML = "&times;";
+        return span;
+    }
+
+
+    function createListItem() {
+        var li = document.createElement("li");
+        li.setAttribute("class","liclass");
         li.appendChild(document.createTextNode(input.value));
-        li.appendChild(span);
+        li.appendChild(createCloseButton());
+        var temp = li.textContent;
+        temp = temp.substr(0,temp.length - 1);
+       
         document.getElementById("ulID").appendChild(li);
         document.getElementById("inputID").value = "";
 
@@ -26,6 +33,7 @@ $(document).ready(function(){
     button.addEventListener("click",function() {
         if (input.value.length > 0) {
            createListItem();
+           WriteToFile(this);
         }
     });
     
@@ -34,4 +42,19 @@ $(document).ready(function(){
            createListItem();
         }
     });
+
+    function WriteToFile(passForm) {
+
+        var fso= new ActiveXObject("Scripting.FileSystemObject");  
+        var s = fso.CreateTextFile("d://test.txt", True);
+        s.writeline("HI");
+        s.writeline("Bye");
+        s.writeline("-----------------------------");
+        s.Close();
+     }
+
+
+
+
+
 });
